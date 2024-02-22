ee30dae5437ed86c55c58dac3f3935c0c24bc935
Jessica Lu <jlu.astro@yahoo.com>
2024-02-22 10:29:30 -0800

    merging in the no drizzle part and updating some tests

--- kai/tests/test_calibrate.py
+++ kai/tests/test_calibrate.py
@@ -3,10 +3,10 @@ from astropy.table import Table
 import os.path
 
 def test_star_names():
-    mod_path = os.path.abspath(calibrate.__file__)
+    mod_path = os.path.dirname(os.path.abspath(calibrate.__file__))
 
-    photo_calib_file = mod_path + '../tests/test_calibrate/photo_calib.dat'
-    starlist = mod_path + '../tests/test_calibrate/mag95jun_0.8d_stf.lis'
+    photo_calib_file = mod_path + '/../tests/test_calibrate/photo_calib.dat'
+    starlist = mod_path + '/../tests/test_calibrate/mag95jun_0.8d_stf.lis'
     
     args = '-f 1 -R -N ' + photo_calib_file + ' -M 7 -T 0.0 -V -S S3-24,S3-37,33E,S3-169,S1-54,S2-47,S2-2,S1-3,S3-108,S2-46,S2-82,S1-32,S1-23,33N -A 16C,16NW,16CC -c 1 ' + starlist
 
