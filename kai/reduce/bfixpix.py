666ebe5f33ee0866dc913f59d5d3f9bc49638827
nsabrams <nsabrams@gmail.com>
2025-06-10 15:29:34 -0700

    added reference pixel comment

--- kai/reduce/bfixpix.py
+++ kai/reduce/bfixpix.py
@@ -17,7 +17,10 @@ def bfixpix(image_file, mask_file, outsuffix='_f', msksuffix='_s'):
         input image file to fix bad pixels on
 
     mask_file : string
-        mask file (0 == good pixels, >0 == bad pixels
+        mask file (0 == good pixels, >0 == bad pixels)
+        Only fixes bad pixels = 1
+        pixel = 2 means that it's a reference pixel and will be 
+        filtered out in other steps but not "fixed."
 
     outsuffix : string
         suffix for fixed image. default = '_f'
